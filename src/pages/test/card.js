d364be01536cd40aafbcf779a0ea63e862fc9ecd
cadst <tyu1234567@naver.com>
2025-11-24 00:31:31 +0900

    Feat: Card 컴포넌트 UI (모바일/PC) 추가

--- src/pages/test/card.js
+++ src/pages/test/card.js
@@ -1,20 +1,19 @@
-import TextCard from '@/components/card/TextCard';
-import ImageCard from '@/components/card/ImageCard';
+import Card from '@/components/card/Card';
 import Column from '@/components/column/Column';
 
 export default function CardTestPage() {
   return (
     <div>
-      <TextCard title="Test Card" tags={['tag1', 'tag2', 'tag3']} date="2021-01-01" />
-      <ImageCard
+      <Card imageUrl={null} title="Test Cssard" tags={['tag1', 'tag2', 'tag3']} date="2021-01-01" />
+      <Card
         imageUrl="/images/card/card_image2.svg"
         title="Test Card"
         tags={['tag1', 'tag2', 'tag3']}
         date="2021-01-01"
       />
-      <Column>
-        <TextCard title="Test Card" tags={['tag1', 'tag2', 'tag3']} date="2021-01-01" />
-        <ImageCard
+      <Column title={'To Do'} cardCount={10}>
+        <Card imageUrl={null} title="Test Card" tags={['tag1', 'tag2', 'tag3']} date="2021-01-01" />
+        <Card
           imageUrl="/images/card/card_image2.svg"
           title="Test Card"
           tags={['tag1', 'tag2', 'tag3']}
