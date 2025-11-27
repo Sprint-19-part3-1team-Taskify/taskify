e3ebdbe764d6a5175a5f236c46398a779983e55b
DESKTOP-9RTNCQK\User <leedongguk0@gmail.com>
2025-11-27 23:40:14 +0900

    Feat: 미로그인시 로그인이 필요한 페이지로 갈 경우 로그인 페이지로 리다이렉트

--- src/pages/mydashboard/[dashboardid].js
+++ src/pages/mydashboard/[dashboardid].js
@@ -1,9 +1,13 @@
 import SideMenu from '@/components/sidemenu/SideMenu';
+import { withAuth } from '@/lib/auth';
 
 export default function MyDashBoard() {
   return <>상세</>;
 }
+
 MyDashBoard.headerType = 'header5';
 MyDashBoard.dashboardName = '내 대시보드';
 MyDashBoard.mainClassName = 'mydashboard';
 MyDashBoard.sidemenuShow = true;
+
+export const getServerSideProps = withAuth();
--- src/pages/mydashboard/index.js
+++ src/pages/mydashboard/index.js
@@ -1,7 +1,12 @@
+import { withAuth } from '@/lib/auth';
+
 export default function MyDashBoard() {
   return <>dashboard</>;
 }
+
 MyDashBoard.headerType = 'header3';
 MyDashBoard.dashboardName = '내 대시보드';
 MyDashBoard.mainClassName = 'mydashboard';
 MyDashBoard.sidemenuShow = true;
+
+export const getServerSideProps = withAuth();
