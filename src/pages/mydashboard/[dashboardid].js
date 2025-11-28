2d32a5a7695738517f1a3ff2eccae3a6d134a105
Choi hee ruk <chlgmlfkr593@naver.com>
2025-11-28 12:51:36 +0900

    🔥 Remove: MyDashBoard 페이지 구현 코드 삭제

--- src/pages/mydashboard/[dashboardid].js
+++
@@ -1,9 +0,0 @@
-import SideMenu from '@/components/sidemenu/SideMenu';
-
-export default function MyDashBoard() {
-  return <>상세</>;
-}
-MyDashBoard.headerType = 'header5';
-MyDashBoard.dashboardName = '내 대시보드';
-MyDashBoard.mainClassName = 'mydashboard';
-MyDashBoard.sidemenuShow = true;
