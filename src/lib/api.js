e773a97a6c25e1e223fad7b7f29ec557809c9163
sylee <linggo01@naver.com>
2025-11-30 02:32:25 +0900

    ✨Feat: 랜딩 페이지 구현

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,17 +9,6 @@ const api = axios.create({
   },
 });
 
-// 🔥 모든 외부 API 요청마다 accessToken 자동 추가
-api.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
-});
-
 // 내부 Next.js API 라우트용 (로그인 등)
 export const authApi = axios.create({
   baseURL: '/api',
